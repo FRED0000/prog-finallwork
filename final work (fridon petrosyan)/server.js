ee4fcb8dadfa3e3af5e7c873881f4d129729044c
user <user@lab2658m.tumo.lab>
2022-03-21 18:51:37 +0400

    5

---
+++ final work (fridon petrosyan)/server.js
@@ -0,0 +1,72 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var fs = require("fs");
+
+app.use(express.static("."));
+
+app.get('/', function (req, res) {
+    res.redirect('index.html');
+});
+server.listen(3000);
+function generator(matLen, gr, grEat, gDain, trap) {
+    let matrix = [];
+    for (let i = 0; i < matLen; i++) {
+        matrix[i] = [];
+        for (let j = 0; j < matLen; j++) {
+            matrix[i][j] = 0;
+        }
+    }
+    for (let i = 0; i < gr; i++) {
+        let x = Math.floor(Math.random() * matLen);
+        let y = Math.floor(Math.random() * matLen);
+        if (matrix[x][y] == 0) {
+            matrix[x][y] = 1;
+        }
+    }
+    for (let i = 0; i < grEat; i++) {
+        let x = Math.floor(Math.random() * matLen);
+        let y = Math.floor(Math.random() * matLen);
+        if (matrix[x][y] == 0) {
+            matrix[x][y] = 2;
+        }
+    }
+
+    for (let i = 0; i < gDain; i++) {
+        let x = Math.floor(Math.random() * matLen);
+        let y = Math.floor(Math.random() * matLen);
+        if (matrix[x][y] == 0) {
+            matrix[x][y] = 3;
+        }
+    }
+    for (let i = 0; i < trap; i++) {
+        let x = Math.floor(Math.random() * matLen);
+        let y = Math.floor(Math.random() * matLen);
+        if (matrix[x][y] == 0) {
+            matrix[x][y] = 4;
+        }
+    }
+
+    return matrix;
+}
+
+let side = 30;
+
+let matrix = generator(30, 600, 50, 30, 15);
+
+io.sockets.emit('send matrix', matrix)
+Grass = require("./Grass")
+GrassEater = require("./GrassEater")
+
+var grassArr = []
+var grassEaterArr = []
+let GuardianArr = []
+var trapArr = []
+this.power2 = Math.floor(Math.random() * 10 + 10);
+
+
+
+io.on('connection', function (socket) {
+    createObject(matrix)
+})
